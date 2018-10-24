d001fa3b4a46024a727c5063a3d5d1830665376d
Rogelio Zamudio <yeyozaz@gmail.com>
2018-10-24 14:25:04 -0500

    modified folder structure

---
+++ docs/precache-manifest.cc0e9ed680e447f1a6c7e18196934f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87e45e16f97c4ff474e52d2a3cf4323",
+    "url": "/static/media/rogelio.d87e45e1.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f392aebd63525a53e170",
+    "url": "/static/js/main.f392aebd.chunk.js"
+  },
+  {
+    "revision": "a5405056eb141231896a",
+    "url": "/static/js/1.a5405056.chunk.js"
+  },
+  {
+    "revision": "f392aebd63525a53e170",
+    "url": "/static/css/main.b3992ad5.chunk.css"
+  },
+  {
+    "revision": "76d2716de3a8701a640dd5cc64efec3d",
+    "url": "/index.html"
+  }
+];
