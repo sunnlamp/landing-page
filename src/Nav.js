445ddc834dadad1f4a8d6eb4c10a334d52ef59a9
Rogelio Zamudio <yeyozaz@gmail.com>
2018-10-25 20:18:43 -0500

    converted Nav to a functional component, modified README by removing excess material provided by create-react-app, retained some

--- src/Nav.js
+++ src/Nav.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Nav extends Component {
-  render() {
-    return (
-      <header>
-        <nav className="navbar">
-          <li>
-            <a href="">
-              <span>About</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Portfolio</span>
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/sunnlamp">
-              <i className="fab fa-github" aria-hidden="true">
-              </i>
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rogelio-zamudio-b78180133/">
-              <i className="fab fa-linkedin-in" aria-hidden="true">
-              </i>
-            </a>
-          </li>
-          <li>
-            <a target="_top" rel="noopener noreferrer" href="mailto:rzamudiojr@gmail.com">
-              <i className="fas fa-envelope" aria-hidden="true">
-              </i>
-            </a>
-          </li>
-        </nav>
-      </header>  
-    )
-  }
-}
+const Nav = () => (
+  <header>
+    <nav className="navbar">
+      <li>
+        <a href="">
+          <span>About</span>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span>Portfolio</span>
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/sunnlamp">
+          <i className="fab fa-github" aria-hidden="true">
+          </i>
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rogelio-zamudio-b78180133/">
+          <i className="fab fa-linkedin-in" aria-hidden="true">
+          </i>
+        </a>
+      </li>
+      <li>
+        <a target="_top" rel="noopener noreferrer" href="mailto:rzamudiojr@gmail.com">
+          <i className="fas fa-envelope" aria-hidden="true">
+          </i>
+        </a>
+      </li>
+    </nav>
+  </header>  
+)
+
+export default Nav
